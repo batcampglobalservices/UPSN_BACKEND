faa222c56b6cad682e2189b9294f73399d94f398
bakpobatombari <batombari12@gmail.com>
2025-11-20 22:48:02 +0100

    Adjust backend Dockerfile for Railway builds

--- backend/settings.py
+++ backend/settings.py
@@ -264,7 +264,7 @@ SIMPLE_JWT = {
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://upsn-website.vercel.app,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173',
+    default='https://upsn-website.vercel.app,http://upsn-website.vercel.app,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173',
     cast=Csv()
 )
 
